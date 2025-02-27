930ca60cbbaab15d04a40e3ecb2ea653dcbf724d
junaid7192 <j92531782@gmail.com>
2025-02-27 16:55:50 +0530

    Update m.py

--- m.py
+++ m.py
@@ -234,7 +234,7 @@ def handle_aadi(message):
                 record_command_logs(user_id, '/aadi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./aadi {target} {port} {time} 975"
+                full_command = f"./aadi {target} {port} {time} 10"
                 subprocess.run(full_command, shell=True)
                 response = f"-ATACK COMPLETE- Target: {target} Port: {port} Port: {time}"
         else:
